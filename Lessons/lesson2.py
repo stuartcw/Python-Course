01aef3bc5f38ad8e7f5ede3f3148d460713dee8c
Stuart Woodward <github.com@stuartwoodward.com>
2023-02-21 16:49:47 +0900

    Create lesson2.py

---
+++ Lessons/lesson2.py
@@ -0,0 +1,12 @@
+def multiply_numbers(a,b):
+  return
+
+# ---
+
+def test_multiply_numbers():
+  if multiply_numbers(3,2) == 6:
+    print("Passed!")
+  else:
+    print("Failed!")
+   
+test_multiply_numbers()
